8cf090b4591062cc7e3f9045e1816bf9735fbce4
Leksa <admin@petiteweb.ru>
2022-12-14 20:41:12 +0300

    add bootstrap-css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-import Logo from "./assets/logo.svg";
+// import Logo from "./assets/logo.svg";
 
 export default () => {
-    return <>
-        <h1>
-            Simple React App
-            <img src={Logo} alt="React"/>
-        </h1>
-    </>
+    return <div className="container">
+        <div className="row gy-2 gx-2">
+            {/* div.col{$}*20 */}
+            <div className="col col-12 py-3 px-5 bg-dark text-light">1</div>
+            <div className="col col-6">2</div>
+            <div className="col col-6">3</div>
+            <div className="col col-12">4</div>
+            <div className="col col-12 col-sm-6 col-md-3 bg-warning">5</div>
+            <div className="col col-12 col-sm-6 col-md-3 bg-warning">6</div>
+            <div className="col col-12 col-sm-6 col-md-3 bg-warning">7</div>
+            <div className="col col-12 col-sm-6 col-md-3 bg-warning">8</div>
+            <div className="col col-1">9</div>
+            <div className="col col-11">10</div>
+            <div className="col col-1">11</div>
+            <div className="col col-4">12</div>
+            <div className="col col-4">13</div>
+            <div className="col col-2">14</div>
+            <div className="col col-1">15</div>
+            <div className="col col-12">16</div>
+        </div>
+    </div>
 }
